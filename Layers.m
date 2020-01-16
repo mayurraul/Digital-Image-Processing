3d9dbcb48e408588eca0a4306f6bc5e5adabf7c0
Mayur Raul <37232810+mayurraul@users.noreply.github.com>
2020-01-16 21:11:16 +0530

    Add files via upload

---
+++ Layers.m
@@ -0,0 +1,28 @@
+clc;
+clear;
+close all;
+
+
+I= imread('Lenna_test_image.png');
+subplot(2,2,1);
+imshow(uint8(I));
+title('Original Image')
+
+R=I;
+R(:,:,[2 3])= 0;
+subplot(2,2,2);
+imshow(uint8(R));
+title('RED layer Removed Image')
+
+G=I;
+G(:,:,[1 3])= 0;
+subplot(2,2,3);
+imshow(uint8(G));
+title('GREEN layer Removed Image')
+
+B=I;
+B(:,:,[1 2]) = 0;
+subplot(2,2,4);
+imshow(uint8(B));
+
+title('BLUE layer Removed Image')
